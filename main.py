39cc0c237c387083acb5da6c2eba5b17a85f3885
ThePrimeagen <the.primeagen@aol.com>
2025-08-01 15:26:30 -0400

    Found an error

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def get_book_text(f_path):
 from stats import get_num_words
 
 def main(f_path):
-      print(f"{word_count(f_path)} words found in the document" )
+      print(f"{get_num_words(f_path)} words found in the document" )
 
 main("books/frankenstein.txt")
