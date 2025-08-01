7584f7e09a1dd9285f689a19a8ccd6a959bde123
Tjd511 <tannerdejesu@gmail.com>
2025-08-01 15:21:58 -0400

    Making Import

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ def get_book_text(f_path):
         file_contents = f.read()
         return file_contents
 
-
-def word_count(f_path):
-    counter = 0
-    words = get_book_text(f_path).split()
-    for word in words:
-        counter += 1
-    return counter
-
+from stats import get_num_words
 
 def main(f_path):
       print(f"{word_count(f_path)} words found in the document" )
